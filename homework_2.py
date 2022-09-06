83fa5e192cc9fffb8106510e99d518ac15c99be6
Ursuzev Boris <BorerKicker@yandex.ru>
2022-09-06 19:46:54 +0300

    added task 5

--- homework_2.py
+++ homework_2.py
@@ -1,4 +1,5 @@
 import random
+from random import *
 """
 Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 Пример:
@@ -42,16 +43,29 @@ import random
 Найдите произведение элементов на указанных позициях. Позиции хранятся в 
 файле file.txt в одной строке одно число.
 """
-number_n = int(input())
-kit_digit = []
-for i in range(number_n):
-    kit_digit.append(random.randint(-number_n, number_n))
-print(kit_digit)
-with open('index_hw2.txt', 'w') as file:
-    for i in range(random.randint(2, number_n)):
-        file.write(str(random.randint(0, len(kit_digit)-1)) + '\n')
-work = 1
-with open('index_hw2.txt', 'r') as index:
-    for i in index:
-        work *= kit_digit[int(i)]
-print(f'Произведение элементов на указанных позициях в файле {work}')
+# number_n = int(input('Введите количество элементов:\n'))
+# kit_digit = []
+# for i in range(number_n):
+#     kit_digit.append(randint(-number_n, number_n))
+# print(kit_digit)
+# with open('index_hw2.txt', 'w') as file:
+#     for i in range(randint(2, number_n)):
+#         file.write(str(randint(0, len(kit_digit)-1)) + '\n')
+# work = 1
+# with open('index_hw2.txt', 'r') as index:
+#     for i in index:
+#         work *= kit_digit[int(i)]
+# print(f'Произведение элементов на указанных позициях в файле {work}')
+
+"""
+Реализуйте алгоритм перемешивания списка.
+"""
+counter = int(input('Введите размер списка:\n'))
+list_number = []
+for i in range(counter):
+    list_number.append(randint(1, 100))
+print(list_number)
+for i in range(counter):
+    index = randint(0, len(list_number)-1)
+    list_number[i], list_number[index] = list_number[index], list_number[i]
+print(list_number)
