a667a76dccab7a67898b21735ec5520eb41cfa02
Ursuzev Boris <BorerKicker@yandex.ru>
2022-09-09 18:12:43 +0300

    added task 3

--- homework_3.py
+++ homework_3.py
@@ -4,7 +4,7 @@
 Пример:
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 """
-# small_list = [2, 5, 5, 9, 3]
+# small_list = [int(input('Enter the element: ')) for _ in range(int(input('Enter the number of element: ')))]
 # sum_item = 0
 # for i in range(len(small_list)):
 #     if i % 2 == 1:
@@ -18,7 +18,7 @@
 - [2, 3, 4, 5, 6] => [12, 15, 16];
 - [2, 3, 5, 6] => [12, 15]
 '''
-list_numbers = [2, 3, 4, 5, 6]
+# list_numbers = [int(input('Enter the element: ')) for _ in range(int(input('Enter the number of element: ')))]
 
 def work_of_steam(kit):
     work_steam = []
@@ -33,5 +33,15 @@ def work_of_steam(kit):
 Пример:
 - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 '''
+list_numbers = [float(input('Enter the element: ')) for _ in range(int(input('Enter the number of element: ')))]
 
+def difference_min_max_factional_part(some_list):
+    new_list = []
+    for i in some_list:
+        sigans = len(str(i).split('.')[1])
+        if i - int(i) != 0:
+            new_list.append(round(i - int(i), sigans))
+    return max(new_list) - min(new_list)
 
+print('The difference between the maximum and minimum fractional part of the element is equal', end=' ')
+print(difference_min_max_factional_part(list_numbers))
