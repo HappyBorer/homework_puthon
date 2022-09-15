f21ee07c62bf623790b96edc77533ae7b0d9573f
Boris <BorerKicker@yandex.ru>
2022-09-15 09:21:35 +0300

    changed the solution of task 2

--- homework_4.py
+++ homework_4.py
@@ -45,30 +45,30 @@ from sympy import *
 Пример:
 - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 '''
-# from random import *
-# natural_degree = int(input('Enter the degree: '))
-# coefficients = [randint(0, 100) for i in range(natural_degree + 1)]
-# polynomial = ''
-# number_roots = len(coefficients[::-1])
-# if number_roots < 1:
-#     polynomials = 'x = 0'
-# else:
-#     for i in range(number_roots):
-#         if i != number_roots - 1 and i != number_roots - 2 and coefficients[i] != 0:
-#             polynomial += f'{coefficients[i]}x^{number_roots - i - 1} '
-#             if coefficients[i + 1] != 0:
-#                 polynomial += '+ '
-#         elif coefficients[i] != 0 and i == number_roots - 2:
-#             polynomial += f'{coefficients[i]}x '
-#             if coefficients[i + 1] != 0:
-#                 polynomial += '+ '
-#         elif coefficients[i] != 0 and i == number_roots - 1:
-#             polynomial += f'{coefficients[i]} = 0'
-#         elif coefficients[i] == 0 and i == number_roots - 1:
-#             polynomial += '= 0'
-#
-# with open('polynomials.txt', 'w') as data:
-#     data.write(polynomial)
+from random import *
+natural_degree = int(input('Enter the degree: '))
+coefficients = [randint(0, 100) for i in range(natural_degree + 1)]
+polynomial = ''
+number_roots = len(coefficients[::-1])
+if number_roots < 1:
+    polynomials = 'x = 0'
+else:
+    for i in range(number_roots):
+        if i != number_roots - 1 and i != number_roots - 2 and coefficients[i] != 0:
+            polynomial += f'{coefficients[i]}x^{number_roots - i - 1} '
+            if coefficients[i + 1] != 0:
+                polynomial += '+ '
+        elif coefficients[i] != 0 and i == number_roots - 2:
+            polynomial += f'{coefficients[i]}x '
+            if coefficients[i + 1] != 0:
+                polynomial += '+ '
+        elif coefficients[i] != 0 and i == number_roots - 1:
+            polynomial += f'{coefficients[i]} = 0'
+        elif coefficients[i] == 0 and i == number_roots - 1:
+            polynomial += '= 0'
+
+with open('polynomials.txt', 'w') as data:
+    data.write(polynomial)
 
 '''
 Даны два файла, в каждом из которых находится запись многочлена. 
